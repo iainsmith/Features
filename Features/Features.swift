879ccccb3f6b4ed1e8f057f99d70c0be669f1c83
Iain <iain@mountain23.com>
2016-07-11 22:23:47 +0100

    Move platform to separate file.

--- Features/Features.swift
+++ Features/Features.swift
@@ -28,85 +28,8 @@ public func ==(lhs: FeatureName, rhs: FeatureName) -> Bool {
     return lhs.rawValue == rhs.rawValue
 }
 
-struct Platform: OptionSetType {
-    let rawValue: Int
-    static let iPhone = Platform(rawValue: 1)
-    static let iPad   = Platform(rawValue: 2)
-    static let tvOS   = Platform(rawValue: 3)
-    static let All: Platform = [.iPhone, .iPad, .tvOS]
-
-
-    init(rawValue: Int) {
-        self.rawValue = rawValue
-    }
-
-    init?(string: String?) {
-        guard let string = string else { return nil }
-
-        switch string.lowercaseString {
-        case "iphone":
-            self = Platform.iPhone
-        case "ipad":
-            self = Platform.iPad
-        case "tvos":
-            self = Platform.tvOS
-        case "all":
-            self = Platform.All
-        default:
-            return nil
-        }
-    }
-
-    static func platFormFromJSONString(jsonString: String?) -> Platform? {
-        guard let jsonString = jsonString else { return Platform.All }
-
-        var platform: Platform = []
-        let string = jsonString.lowercaseString
-        ["iphone", "ipad", "tvos"].forEach { member in
-            if string.containsString(member) {
-                if let _ = Platform(string: member) {
-                    platform.insert(Platform())
-                }
             }
         }
-
-        if platform.isEmpty {
-            return nil
-        }
-
-        return platform
-    }
-
-    static func currentDevice() -> Platform {
-        switch UIDevice.currentDevice().userInterfaceIdiom {
-        case .TV:
-            return .tvOS
-        case .Phone:
-            return .iPhone
-        case .Pad:
-            return .iPad
-        default:
-            return .iPhone
-        }
-    }
-}
-
-extension Platform: CustomDebugStringConvertible {
-    var debugDescription: String {
-        var platforms = [String]()
-        if self.contains(.iPhone) {
-            platforms.append("iPhone")
-        }
-
-        if self.contains(.iPad) {
-            platforms.append("iPad")
-        }
-
-        if self.contains(.tvOS) {
-            platforms.append("TVOS")
-        }
-
-        return platforms.joinWithSeparator(", ")
     }
 }
 

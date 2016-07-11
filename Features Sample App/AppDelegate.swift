299612428bb006d85d7f534745964322aa957ebc
Iain <iain@mountain23.com>
2016-07-11 17:15:19 +0100

    Add sample app skeleton

---
+++ Features Sample App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Features Sample App
+//
+//  Created by iainsmith on 11/07/2016.
+//  Copyright © 2016 M23. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}

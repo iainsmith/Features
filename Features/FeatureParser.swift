ae98761ef67e11881f025d0e4f7962597a332bd9
Iain <iain@mountain23.com>
2016-09-23 13:39:13 +0100

    Add support for downloading and saving remote feature flags.

--- Features/FeatureParser.swift
+++ Features/FeatureParser.swift
@@ -8,20 +8,49 @@
 
 import Foundation
 
+typealias FeatureJSON = Array<Dictionary<String, AnyObject>>
+
 internal struct FeatureParser {
-    static func loadFromDisk(percentage percentage: UInt = FeatureService.percentage(), bundle: NSBundle = FeatureService.bundle, fileName: String = FeatureService.fileName) -> FeatureStore {
+    static func loadFromDisk(percentage percentage: UInt = FeatureService.devicePercentage(), bundle: NSBundle = FeatureService.bundle, fileName: String = FeatureService.fileName) -> FeatureStore {
         let path = bundle.pathForResource(fileName, ofType: "json")!
-        let data = NSData(contentsOfFile: path)!
-        let featuresJSON: Array<Dictionary<String, AnyObject>> = try! NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as! Array<Dictionary<String, AnyObject>>
-
-        let features = featuresJSON.flatMap { feature -> Feature? in
-            FeatureParser.dictionaryToFeature(feature)
-        }
-
-        return FeatureStore(features: features, devicePercentage: percentage)
+        return loadFromPath(path, percentage: percentage)!
     }
 
-    private static func dictionaryToFeature(dictionary: Dictionary<String, AnyObject>) -> Feature? {
+    static func loadFromPath(path: String, percentage: UInt = FeatureService.devicePercentage(), strict: Bool = false) -> FeatureStore? {
+        guard let data = NSData(contentsOfFile: path) else { return nil }
+        let result = loadFromData(data, percentage: percentage, strict: strict)
+        switch result {
+        case .Success(let store, _):
+            return store
+        case .Failure:
+            return nil
+        }
+    }
+
+    static func loadFromData(data: NSData, percentage: UInt = FeatureService.devicePercentage(), strict: Bool) -> FeatureResult<(FeatureStore, NSData)> {
+        do {
+            if let featuresJSON: Array<Dictionary<String, AnyObject>> = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as? Array<Dictionary<String, AnyObject>> {
+
+                let features = featuresJSON.flatMap { feature -> Feature? in
+                    FeatureParser.dictionaryToFeature(feature)
+                }
+
+                if strict == false || featuresJSON.count == features.count {
+                    let store = FeatureStore(features: features, devicePercentage: percentage)
+                    return .Success(store, data)
+                } else {
+                    return .Failure()
+                }
+
+            } else {
+                return .Failure()
+            }
+        } catch {
+            return .Failure()
+        }
+    }
+
+    static func dictionaryToFeature(dictionary: Dictionary<String, AnyObject>) -> Feature? {
         let percentageKey = "percentage"
         let platformKey = "platforms"
         let nameKey = "name"
--- Features/FeatureService.swift
+++ Features/FeatureService.swift
@@ -14,8 +14,10 @@ public struct FeatureService {
     public static var fileName = "features"
     public static var bundle = NSBundle.mainBundle()
     public static var overRidePercentage: UInt? = nil
-    public static var featureStore: FeatureStore = FeatureParser.loadFromDisk()
     public static var store: FeaturePercentageStore = NSUserDefaults.standardUserDefaults()
+    public static var featureStore: FeatureStore = FeatureService.latestSavedStore()
+    private static var remoteClient = FeatureClient()
+
 
     public static func featureDetails() -> [FeatureData] {
         return featureStore.features.map { feature in
@@ -30,10 +32,73 @@ public struct FeatureService {
         featureStore = FeatureStore(features: featureStore.features, devicePercentage: percentage)
     }
 
-    internal static func percentage() -> UInt {
+    public static func devicePercentage() -> UInt {
        return overRidePercentage ?? existingPercentage() ?? generatedSavedPercentage()
     }
 
+    public static func updateRemoteFeaturesWithRequest(request: NSURLRequest) {
+        remoteClient.updateFeatures(request) { result in
+            switch result {
+            case .Success(let store, let data):
+                saveAndUpdateFeatureStore(store, data: data)
+            case .Failure:
+                print("failed to update features from request")
+            }
+        }
+    }
+}
+
+
+extension FeatureService {
+    private static func latestSavedStore() -> FeatureStore {
+        return latestRemoteStore() ?? latestLocalStore()
+    }
+
+    private static func latestRemoteStore() -> FeatureStore? {
+        // Use strict mode so consumers are less likely to break things.
+        return FeatureParser.loadFromPath(remoteFilePath(), strict: true)
+    }
+
+    private static func remoteFolderPath() -> String {
+        return NSSearchPathForDirectoriesInDomains(.ApplicationSupportDirectory, .UserDomainMask, true)[0]
+    }
+
+    private static func remoteFilePath() -> String {
+        var supportPath = remoteFolderPath()
+        supportPath.appendContentsOf("/features.json")
+        return supportPath
+    }
+
+    private static func latestLocalStore() -> FeatureStore {
+        return FeatureParser.loadFromDisk()
+    }
+
+    private static func updateLatestJSONWithData(data: NSData) throws {
+        let fileManager = NSFileManager.defaultManager()
+        let folderPath = remoteFolderPath()
+
+        if fileManager.fileExistsAtPath(folderPath) == false {
+            try fileManager.createDirectoryAtPath(folderPath, withIntermediateDirectories: true, attributes: nil)
+        }
+
+        let filePath = remoteFilePath()
+        try data.writeToFile(filePath, options: [.DataWritingAtomic])
+        print("data saved to \(filePath)")
+
+        try data.writeToFile(remoteFilePath(), options: [])
+    }
+
+    private static func saveAndUpdateFeatureStore(store: FeatureStore, data: NSData) {
+        do {
+            try updateLatestJSONWithData(data)
+        } catch {
+            print("failed to write features to disk")
+        }
+    }
+}
+
+// MARK: - Device Percentages
+extension FeatureService {
     private static func existingPercentage() -> UInt? {
         return store.feature_storedPercentage
     }
